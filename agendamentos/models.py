fc329ec980faade58251af17888361f8c35f11c1
RenanSPradoo <renansilvaprado2@hotmail.com>
2024-09-16 15:54:20 -0300

    Test: Inclusão de um arquivo.

--- agendamentos/models.py
+++ agendamentos/models.py
@@ -8,12 +8,11 @@ class AgendamentoPagamento(models.Model):
     status_recorrencia = models.CharField(max_length=50)
     agencia = models.PositiveIntegerField()
     conta = models.PositiveIntegerField()
-    valor_pagamento = models.IntegerField()  # Armazenado como inteiro
+    valor_pagamento = models.IntegerField()  
 
     def save(self, *args, **kwargs):
-        # Convertendo o valor_pagamento de decimal para inteiro antes de salvar
         if isinstance(self.valor_pagamento, float):
-            self.valor_pagamento = int(self.valor_pagamento * 100)  # Corrigido: multiplicar por 100
+            self.valor_pagamento = int(self.valor_pagamento * 100)  
         super(AgendamentoPagamento, self).save(*args, **kwargs)
 
     def __str__(self):
